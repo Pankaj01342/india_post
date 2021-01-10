8cb61c931bced06a520921c1d47fac764239b90b
Pankaj01342 <pankajkumary01342@gmail.com>
2021-01-09 22:58:07 -0800

    my first commit reactapp

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Ippb from './Ippb';
+import PostalIn from './Postal_in';
+import Ssa from './Ssa';
+import Sb from './Sb';
+import Mis from './Mis';
+import Dakpay from './Dakpay';
+import Footer from './Footer';
+
+
+import { BrowserRouter as Router,Switch,Route } from "react-router-dom"
+
+function App() {
+  return (
+  	<Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/ippb">
+            <Ippb />
+          </Route>
+
+          <Route path="/postal_insurance">
+            <PostalIn />
+          </Route>
+
+          <Route path="/ssa">
+            <Ssa />
+          </Route>
+
+          <Route path="/sb">
+            <Sb />
+          </Route>
+
+          <Route path="/mis">
+            <Mis />
+          </Route>
+
+          <Route path="/dakpay">
+            <Dakpay />
+          </Route>
+  
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBw1krPyABEMinPJCEESmAzIH-mZmFDWc0",
+  authDomain: "india-post-clone.firebaseapp.com",
+  projectId: "india-post-clone",
+  storageBucket: "india-post-clone.appspot.com",
+  messagingSenderId: "597905088107",
+  appId: "1:597905088107:web:fa4812711e9f77cfc8db2e",
+  measurementId: "G-PX6XH5NTWH"
+};
